6e0180edcb210c7f19317fa4adb454df4c199994
dcdholder <dcdholder@gmail.com>
2017-10-06 16:55:19 -0400

    Updated the public API to accept a ticker symbol, start date, end date as URI arguments.

--- publicApi.py
+++ publicApi.py
@@ -1,32 +1,38 @@
 import financialDataIntervals
 
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, marshal_with, reqparse
 
 import json
 
 app = Flask(__name__)
 api = Api(app)
 
+requestParser = reqparse.RequestParser()
+
+requestParser.add_argument('symbol',    dest='symbol',    type=str, location='args', required=True, help='A ticker symbol is required.')
+requestParser.add_argument('startdate', dest='startdate', type=str, location='args', required=True, help='A start date is required.')
+requestParser.add_argument('enddate',   dest='enddate',   type=str, location='args', required=True, help='An end date is required.')
+
 class StockIntervalPublicResource(Resource):
-    def get(self, timeGranularity, tickerSymbol):
+    def get(self):
+        args = requestParser.parse_args()
+
         with open("conf.json") as data_file:
             conf = json.load(data_file)
 
         with open("tickerSymbols.json") as data_file:
             tickerSymbols = json.load(data_file)
 
-        if tickerSymbol in tickerSymbols:
-            [resolvedStartDateString, resolvedEndDateString] = financialDataIntervals.resolveToDateRange(startDateString,endDateString,timeGranularity)
-
+        if args.symbol in tickerSymbols:
             try:
-                return financialDataIntervals.getExistingIntervalData(tickerSymbol,resolvedStartDateString,resolvedEndDateString)
-            except IOError:
-                return tickerSymbol + ' data for the range ' + startDateString + 'to' + endDateString + ' could not be found.', 404
+                return financialDataIntervals.getIntervalDataFromDateRange(args.symbol,args.startdate,args.enddate), 200
+            except Exception as e:
+                return str(e), 404
         else:
             return 'No data is available for ' + tickerSymbol + '.', 404
 
-api.add_resource(StockIntervalPublicResource, '/intervals/<string:timeGranularity>/<string:tickerSymbol>')
+api.add_resource(StockIntervalPublicResource, '/intervals')
 
 if __name__ == '__main__':
     app.run(debug=True)
